0c305c9560b10306bdacec679eb6b13f5878c7fc
0AliReza0 <alireza.abedi9310@gmail.com>
2023-07-28 22:53:41 +0330

    fake data added...

---
+++ src/data.ts
@@ -0,0 +1,23 @@
+const data: Data = {
+  report: [],
+};
+
+interface Data {
+  report: {
+    id: string;
+    source: string;
+    amount: number;
+    created_at: Date;
+    updated_at: Date;
+    type: 'expense' | 'income';
+  }[];
+}
+
+data.report.push({
+  id: 'uuid',
+  source: 'Salary',
+  amount: 7500,
+  created_at: new Date(),
+  updated_at: new Date(),
+  type: 'expense',
+});
