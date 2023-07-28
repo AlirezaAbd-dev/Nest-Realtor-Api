db84581895c723d81a5deea7808cf17b6d916c58
0AliReza0 <alireza.abedi9310@gmail.com>
2023-07-28 23:53:33 +0330

    all controllers created...

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,29 +1,87 @@
-import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Body,
+  HttpCode,
+} from '@nestjs/common';
+import { v4 as uuid } from 'uuid';
+
+import { ReportType, data } from './data';
 
 @Controller('report/:type')
 export default class AppController {
   @Get('')
-  getAllReports() {
-    return [];
+  getAllReports(@Param('type') type: string) {
+    const reportType =
+      type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
+    return data.report.filter((report) => report.type === reportType);
   }
 
   @Get(':id')
-  getReportById() {
-    return {};
+  getReportById(@Param('type') type: string, @Param('id') id: string) {
+    const reportType =
+      type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
+    return data.report
+      .filter((report) => report.type === reportType)
+      .find((report) => report.id === id);
   }
 
   @Post()
-  createReport() {
-    return 'created';
+  createReport(
+    @Body() { amount, source }: { amount: number; source: string },
+    @Param('type') type: string,
+  ) {
+    const newReport = {
+      id: uuid(),
+      source,
+      amount,
+      created_at: new Date(),
+      updated_at: new Date(),
+      type: type === 'income' ? ReportType.INCOME : ReportType.EXPENSE,
+    };
+    data.report.push(newReport);
+
+    return newReport;
   }
 
   @Put(':id')
-  updateReport() {
-    return 'updated';
+  updateReport(
+    @Body() body: { amount: number; source: string },
+    @Param('type') type: string,
+    @Param('id') id: string,
+  ) {
+    const reportType =
+      type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
+    const report = data.report
+      .filter((report) => report.type === reportType)
+      .find((report) => report.id === id);
+
+    if (!report) {
+      return;
+    }
+
+    const reportIndex = data.report.findIndex((r) => r.id === report.id);
+
+    data.report[reportIndex] = {
+      ...data.report[reportIndex],
+      ...body,
+    };
+
+    return data.report[reportIndex];
   }
 
+  @HttpCode(204)
   @Delete(':id')
-  deleteReport() {
-    return 'deleted';
+  deleteReport(@Param('id') id: string) {
+    const reportIndex = data.report.findIndex((report) => report.id === id);
+
+    if (reportIndex === -1) return;
+
+    data.report.splice(reportIndex, 1);
+    return;
   }
 }
--- src/data.ts
+++ src/data.ts
@@ -1,5 +1,35 @@
-const data: Data = {
-  report: [],
+export enum ReportType {
+  INCOME = 'income',
+  EXPENSE = 'expense',
+}
+
+export const data: Data = {
+  report: [
+    {
+      id: 'uuid1',
+      source: 'Salary',
+      amount: 7500,
+      created_at: new Date(),
+      updated_at: new Date(),
+      type: ReportType.INCOME,
+    },
+    {
+      id: 'uuid2',
+      source: 'Youtube',
+      amount: 2500,
+      created_at: new Date(),
+      updated_at: new Date(),
+      type: ReportType.INCOME,
+    },
+    {
+      id: 'uuid3',
+      source: 'Food',
+      amount: 500,
+      created_at: new Date(),
+      updated_at: new Date(),
+      type: ReportType.EXPENSE,
+    },
+  ],
 };
 
 interface Data {
@@ -9,15 +39,6 @@ interface Data {
     amount: number;
     created_at: Date;
     updated_at: Date;
-    type: 'expense' | 'income';
+    type: ReportType;
   }[];
 }
-
-data.report.push({
-  id: 'uuid',
-  source: 'Salary',
-  amount: 7500,
-  created_at: new Date(),
-  updated_at: new Date(),
-  type: 'expense',
-});
