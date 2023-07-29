7ede67d29ec182ae64b462392b8d459ff6a862a9
0AliReza0 <alireza.abedi9310@gmail.com>
2023-07-29 20:09:07 +0330

    summary service created...

--- src/summary/summary.module.ts
+++ src/summary/summary.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { SummaryController } from './summary.controller';
+import { SummaryService } from './summary.service';
 
 @Module({
-  controllers: [SummaryController]
+  controllers: [SummaryController],
+  providers: [SummaryService]
 })
 export class SummaryModule {}
