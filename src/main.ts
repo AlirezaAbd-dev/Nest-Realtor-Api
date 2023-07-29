78ad1beec017b60fd359f7195352fd4dac88a06a
0AliReza0 <alireza.abedi9310@gmail.com>
2023-07-29 13:11:30 +0330

    white list option added to ValidationPipe...

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
