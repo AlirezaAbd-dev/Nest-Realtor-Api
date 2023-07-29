1fdc7c8ba52f5fba64cf4c8010c210befafe083e
0AliReza0 <alireza.abedi9310@gmail.com>
2023-07-29 20:46:20 +0330

    calculateSummary service created...

--- src/summary/summary.service.ts
+++ src/summary/summary.service.ts
@@ -7,7 +7,13 @@ export class SummaryService {
   constructor(private readonly reportService: ReportService) {}
 
   calculateSummary() {
-    this.reportService.getAllReports(ReportType.INCOME);
-    return;
+    const totalExpense = this.reportService
+      .getAllReports(ReportType.EXPENSE)
+      .reduce((sum, report) => sum + report.amount, 0);
+    const totalIncome = this.reportService
+      .getAllReports(ReportType.INCOME)
+      .reduce((sum, report) => sum + report.amount, 0);
+
+    return { totalIncome, totalExpense, netIncome: totalIncome - totalExpense };
   }
 }
