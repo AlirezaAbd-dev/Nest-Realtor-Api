1bab5c18aae3166875f60924382d0414873c289a
0AliReza0 <alireza.abedi9310@gmail.com>
2023-07-30 13:34:34 +0330

    prisma module created...

--- realtor-app/src/user/user.module.ts
+++ realtor-app/src/user/user.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth/auth.controller';
 import { AuthService } from './auth/auth.service';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
+  imports: [PrismaService],
   controllers: [AuthController],
-  providers: [AuthService]
+  providers: [AuthService],
 })
 export class UserModule {}
