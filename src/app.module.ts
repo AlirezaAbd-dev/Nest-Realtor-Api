f395aaabf57ab47fedd358aa2868b5a5e91cb716
0AliReza0 <alireza.abedi9310@gmail.com>
2023-08-01 20:55:15 +0330

    MODIFIED:   app and home modules refactored...

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,26 +1,30 @@
-import { Module } from '@nestjs/common';
+import { Module, Provider } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { HomeModule } from './home/home.module';
 import { APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
-import { UserInterceptor } from './user/interceptors/user.interceptor';
-import { AuthGuard } from './guards/auth.guard';
+import { UserInterceptor as UserInterceptorClass } from './user/interceptors/user.interceptor';
+import { AuthGuard as AuthGuardClass } from './guards/auth.guard';
+
+const UserInterceptor: Provider = {
+  provide: APP_INTERCEPTOR,
+  useClass: UserInterceptorClass,
+}
+
+const AuthGuard: Provider = {
+  provide: APP_GUARD,
+  useClass: AuthGuardClass,
+}
 
 @Module({
   imports: [UserModule, PrismaModule, HomeModule],
   controllers: [AppController],
   providers: [
     AppService,
-    {
-      provide: APP_INTERCEPTOR,
-      useClass: UserInterceptor,
-    },
-    {
-      provide: APP_GUARD,
-      useClass: AuthGuard
-    }
+    UserInterceptor,
+    AuthGuard
   ],
 })
 export class AppModule { }
--- src/home/home.module.ts
+++ src/home/home.module.ts
@@ -1,18 +1,20 @@
-import { ClassSerializerInterceptor, Module } from '@nestjs/common';
+import { ClassSerializerInterceptor, Module, Provider } from '@nestjs/common';
 import { HomeController } from './home.controller';
 import { HomeService } from './home.service';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { APP_INTERCEPTOR } from '@nestjs/core';
+
+const SerializerInterceptor: Provider = {
+  provide: APP_INTERCEPTOR,
+  useClass: ClassSerializerInterceptor,
+}
 
 @Module({
   imports: [PrismaModule],
   controllers: [HomeController],
   providers: [
     HomeService,
-    {
-      provide: APP_INTERCEPTOR,
-      useClass: ClassSerializerInterceptor,
-    },
+    SerializerInterceptor
   ],
 })
-export class HomeModule {}
+export class HomeModule { }
