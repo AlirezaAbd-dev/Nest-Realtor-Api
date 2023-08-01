9e56317058b4d081d88fdbcabdbcad47c42ba8e7
0AliReza0 <alireza.abedi9310@gmail.com>
2023-08-01 20:13:26 +0330

    MODIFIED: fastify adapter added instead of express

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -13,6 +18,7 @@ async function bootstrap() {
       },
     }),
   );
+
   await app.listen(8081);
 }
 bootstrap();
