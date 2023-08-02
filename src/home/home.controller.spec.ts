c70271879c4346285306cbce8d836b7840155f3a
0AliReza0 <alireza.abedi9310@gmail.com>
2023-08-02 22:03:10 +0330

    TEST: new tests for home controller...

--- src/home/home.controller.spec.ts
+++ src/home/home.controller.spec.ts
@@ -1,5 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { HomeController } from './home.controller';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { HomeService } from './home.service';
 
 describe('HomeController', () => {
   let controller: HomeController;
@@ -7,6 +9,7 @@ describe('HomeController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [HomeController],
+      providers: [HomeService, PrismaService]
     }).compile();
 
     controller = module.get<HomeController>(HomeController);
